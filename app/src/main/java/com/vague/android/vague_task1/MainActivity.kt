ae408ac5c73800405d89b8dea2f36dbf410cf1da
Vague <vaguemail369@gmail.com>
2019-09-16 14:45:03 +0100

    Remove the App bar

--- app/src/main/java/com/vague/android/vague_task1/MainActivity.kt
+++ app/src/main/java/com/vague/android/vague_task1/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.vague.android.vague_task1
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-import com.mukesh.tinydb.TinyDB
-
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
     }
 
