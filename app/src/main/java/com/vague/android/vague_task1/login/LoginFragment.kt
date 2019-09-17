b060a2b09465e6ee5ef7f65b2cf6575dcd0f5d3b
Vague <vaguemail369@gmail.com>
2019-09-17 19:01:32 +0100

    Create and attach the login fragment

---
+++ app/src/main/java/com/vague/android/vague_task1/login/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.vague.android.vague_task1.login
+
+import androidx.fragment.app.Fragment
+import com.vague.android.vague_task1.R
+
+class LoginFragment : Fragment(R.layout.fragment_login)
