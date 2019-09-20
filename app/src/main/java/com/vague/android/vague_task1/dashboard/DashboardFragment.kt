c8186bd330911742d2ffb950ae08d19e21d3faf5
Vague <vaguemail369@gmail.com>
2019-09-20 18:31:09 +0100

    Create Dashboard Fragment and attach it to navigation

---
+++ app/src/main/java/com/vague/android/vague_task1/dashboard/DashboardFragment.kt
@@ -0,0 +1,6 @@
+package com.vague.android.vague_task1.dashboard
+
+import androidx.fragment.app.Fragment
+import com.vague.android.vague_task1.R
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard)
