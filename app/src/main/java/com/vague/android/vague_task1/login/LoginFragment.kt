5c00ad9a7793e4716e5f02ee2b4bb9ce0ded789e
Vague <vaguemail369@gmail.com>
2019-09-20 09:00:49 +0100

    Implement design for Fragment Login

--- app/src/main/java/com/vague/android/vague_task1/login/LoginFragment.kt
+++ app/src/main/java/com/vague/android/vague_task1/login/LoginFragment.kt
@@ -1,6 +1,34 @@
 package com.vague.android.vague_task1.login
 
+import android.os.Build
+import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.vague.android.vague_task1.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
-class LoginFragment : Fragment(R.layout.fragment_login)
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Change the color composition of displayed app name
+        val appName = getString(R.string.app_actual_name)
+
+        val spannableStringBuilder = SpannableStringBuilder(appName)
+        val fcsSecondary = when {
+            Build.VERSION.SDK_INT >= 23 -> ForegroundColorSpan(
+                resources.getColor(
+                    R.color.secondaryColor,
+                    activity?.theme
+                )
+            )
+            else -> ForegroundColorSpan(resources.getColor(R.color.secondaryColor))
+        }
+        spannableStringBuilder.setSpan(fcsSecondary, 4, 11, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        txtAppName.text = spannableStringBuilder
+    }
+}
